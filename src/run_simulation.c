e456596cbe9b2ba20ce914bee1f34192cf2e4945
lgandari <lgandaf@gmail.com>
2024-12-08 19:35:50 +0100

    Initial one_philo_simulation(), need to protect it.

--- src/run_simulation.c
+++ src/run_simulation.c
@@ -12,12 +12,28 @@
 
 #include "../inc/philo.h"
 
+void	one_philo_simulation(t_simulation *simulation)
+{
+	t_philo	*philo;
+	long	start_time;
+	long	time_to_die;
+
+	philo = &simulation->philos[0];
+	start_time = gettime(MILLISECONDS);
+	set_long(&simulation->mutex, &simulation->start_time, start_time);
+	set_long(&philo->mutex, &philo->last_meal_time, start_time);
+	time_to_die = get_long(&simulation->mutex, &simulation->time_to_die);
+	usleep(time_to_die);
+	set_bool(&simulation->mutex, &simulation->end, true);
+	write_action(DYING, simulation, philo);
+}
+
 void	run_simulation(t_simulation *simulation)
 {
 	if (simulation->limit_meals == 0)
 		return ;
 	else if (simulation->num_philos == 1)
-		printf("One philo not implemented yet\n");
+		one_philo_simulation(simulation);
 	else
 		printf("Multiple philos not implemented yet\n");
 }
